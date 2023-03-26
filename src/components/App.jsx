86f0b292b9f7fad881eee8d7754acbe119e1d092
Butcherok <butcherok231@gmail.com>
2023-03-26 21:17:44 +0300

    clean stats

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,17 +3,17 @@ import { nanoid } from 'nanoid';
 import { ContactForm } from './contactForm/contactForm';
 import { Filter } from './filter/filter';
 import { ContactList } from './contactList/contactList';
-import { ContactsTitle, Container, Phonebook, PhonebookTitle } from './App.styled'; 
+import {
+  ContactsTitle,
+  Container,
+  Phonebook,
+  PhonebookTitle,
+} from './App.styled';
 // model.id = nanoid(); //=> "V1StGXR8_Z5jdHi6B-myT"
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
